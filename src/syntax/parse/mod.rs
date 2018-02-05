25e650c987fd740f7ca7d55c853eed91210af44b
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2018-02-05 13:55:35 +1100

    Move pi type reparsing out of grammar

--- src/syntax/parse/mod.rs
+++ src/syntax/parse/mod.rs
@@ -4,6 +4,8 @@ use std::str::FromStr;
 
 use syntax::concrete;
 
+use lalrpop_util::ParseError as LalrpopError;
+
 mod grammar {
     include!(concat!(env!("OUT_DIR"), "/syntax/parse/grammar.rs"));
 }
@@ -40,6 +42,50 @@ impl FromStr for concrete::Term {
 
     fn from_str(src: &str) -> Result<concrete::Term, ParseError> {
         grammar::parse_Term(src).map_err(|e| ParseError(format!("{}", e)))
+    }
+}
+
+/// This is an ugly hack that cobbles together a pi type from a binder term and
+/// a body. See the comments on the `PiTerm` rule in the `grammer.lalrpop` for
+/// more information.
+fn reparse_pi_type_hack<L, T>(
+    binder: concrete::Term,
+    body: concrete::Term,
+) -> Result<concrete::Term, LalrpopError<L, T, &'static str>> {
+    use syntax::concrete::Term;
+
+    fn param_names<L, T>(
+        term: Term,
+        names: &mut Vec<String>,
+    ) -> Result<(), LalrpopError<L, T, &'static str>> {
+        match term {
+            Term::Var(name) => names.push(name),
+            Term::App(fn_expr, arg) => {
+                param_names(*fn_expr, names)?;
+                param_names(*arg, names)?;
+            },
+            _ => {
+                return Err(LalrpopError::User {
+                    error: "identifier expected in pi type", // TODO: better error!
+                });
+            },
+        }
+        Ok(())
+    }
+
+    match binder {
+        Term::Parens(term) => {
+            let term = *term; // HACK: see https://github.com/rust-lang/rust/issues/16223
+            match term {
+                Term::Ann(params, ann) => {
+                    let mut names = Vec::new();
+                    param_names(*params, &mut names)?;
+                    Ok(Term::Pi(names, ann, body.into()))
+                },
+                ann => Ok(Term::Arrow(Term::Parens(ann.into()).into(), body.into())),
+            }
+        },
+        ann => Ok(Term::Arrow(ann.into(), body.into())),
     }
 }
 
