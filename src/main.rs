e71ebdfdab84b531c56f5c622e0a0e025756b6ce
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2018-02-13 16:44:19 +1100

    Turn the repl into a common entrypoint

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 extern crate lambdapi;
 extern crate rustyline;
+#[macro_use]
+extern crate structopt;
 
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
 use lambdapi::semantics;
 use lambdapi::syntax::parse;
@@ -10,7 +14,42 @@ use lambdapi::syntax::parse;
 const PROMPT: &str = "λΠ> ";
 const REPL_HISTORY_FILE: &str = "repl-history";
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "lambdapi")]
+enum Opt {
+    /// Check the that the give files type check
+    #[structopt(name = "check")]
+    Check {
+        /// Files to check
+        #[structopt(name = "FILE", parse(from_os_str))]
+        files: Vec<PathBuf>,
+    },
+
+    /// A REPL for running expressions
+    #[structopt(name = "repl")]
+    Repl {
+        /// Files to preload into the REPL
+        #[structopt(name = "FILE", parse(from_os_str))]
+        files: Vec<PathBuf>,
+    },
+}
+
 fn main() {
+    match Opt::from_args() {
+        Opt::Check { files } => run_check(files),
+        Opt::Repl { files } => run_repl(files),
+    }
+}
+
+fn run_check(_files: Vec<PathBuf>) {
+    // TODO: Load files
+
+    unimplemented!()
+}
+
+fn run_repl(_files: Vec<PathBuf>) {
+    // TODO: Load files
+
     let mut rl = Editor::<()>::new();
 
     if let Err(_) = rl.load_history(REPL_HISTORY_FILE) {}
@@ -26,7 +65,7 @@ fn main() {
             Ok(line) => {
                 rl.add_history_entry(&line);
 
-                match run_repl(&line) {
+                match step_repl(&line) {
                     Ok(()) => {},
                     Err(ReplError::Parse(err)) => println!("parse error: {}", err),
                     Err(ReplError::Type(err)) => println!("type error: {:?}", err),
@@ -53,7 +92,7 @@ fn main() {
     rl.save_history(REPL_HISTORY_FILE).unwrap();
 }
 
-fn run_repl(line: &str) -> Result<(), ReplError> {
+fn step_repl(line: &str) -> Result<(), ReplError> {
     use lambdapi::semantics;
     use lambdapi::syntax::concrete::ReplCommand;
     use lambdapi::syntax::core::{Context, RcTerm};
