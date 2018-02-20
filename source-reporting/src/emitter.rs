1b905b2cb107cf0fdc4fc2111b5cf227c48c47fd
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2018-02-20 12:50:29 +1100

    Fix panic

--- source-reporting/src/emitter.rs
+++ source-reporting/src/emitter.rs
@@ -5,13 +5,19 @@ use Diagnostic;
 pub fn emit(codemap: &CodeMap, diagnostic: &Diagnostic) {
     println!("{}: {}", diagnostic.severity, diagnostic.message);
     for span_label in &diagnostic.spans {
-        let file = codemap.find_file(span_label.span.lo()).expect("find_file");
-        let (line, col) = file.location(span_label.span.lo()).expect("location");
+        match codemap.find_file(span_label.span.lo()) {
+            None => if let Some(ref label) = span_label.label {
+                println!("- {}", label)
+            },
+            Some(file) => {
+                let (line, col) = file.location(span_label.span.lo()).expect("location");
 
-        print!("- {}:{}:{}", file.name(), line.number(), col.number());
-        match span_label.label {
-            None => println!(),
-            Some(ref label) => println!(": {}", label),
+                print!("- {}:{}:{}", file.name(), line.number(), col.number());
+                match span_label.label {
+                    None => println!(),
+                    Some(ref label) => println!(": {}", label),
+                }
+            },
         }
     }
 }
