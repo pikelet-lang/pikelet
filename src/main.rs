23eb840dc858582ff89d6e9f111b79f371456ec2
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2018-05-11 08:14:07 +1000

    Return Result from main

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,11 @@
+extern crate failure;
 extern crate pikelet;
 extern crate structopt;
 
+use failure::Error;
 use pikelet::cli::Opts;
-use std::process;
 use structopt::StructOpt;
 
-const EXIT_OK: i32 = 0;
-
-#[cfg(os = "windows")]
-const EXIT_ERR: i32 = 0x0100;
-#[cfg(not(os = "windows"))]
-const EXIT_ERR: i32 = 1;
-
-fn main() {
-    process::exit(match pikelet::cli::run(Opts::from_args()) {
-        Ok(()) => EXIT_OK,
-        Err(err) => {
-            eprintln!("{}", err);
-            EXIT_ERR
-        },
-    })
+fn main() -> Result<(), Error> {
+    pikelet::cli::run(Opts::from_args())
 }
