c15566c3b716be1e8641dde79e6c7a92b91991b4
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2018-08-03 10:25:36 +1000

    Move TcEnv to submodule

--- src/semantics/mod.rs
+++ src/semantics/mod.rs
@@ -5,7 +5,6 @@
 //! For more information, check out the theory appendix of the Pikelet book.
 
 use codespan::ByteSpan;
-use im::HashMap;
 use moniker::{Binder, BoundTerm, Embed, FreeVar, Nest, Scope, Var};
 
 use syntax::core::{
@@ -16,72 +15,15 @@ use syntax::raw;
 use syntax::translation::Resugar;
 use syntax::Level;
 
+mod env;
 mod errors;
 mod prim;
 #[cfg(test)]
 mod tests;
 
+pub use self::env::TcEnv;
 pub use self::errors::{InternalError, TypeError};
 pub use self::prim::{PrimEnv, PrimFn};
-
-/// The type checking environment
-///
-/// A default environment with entries for built-in types is provided via the
-/// implementation of the `Default` trait.
-///
-/// We use persistent data structures internally so that we can copy the
-/// environment as we enter into scopes, without having to deal with the
-/// error-prone tedium of working with mutable context.
-#[derive(Clone, Debug)]
-pub struct TcEnv {
-    /// Primitive definitions
-    pub primitives: PrimEnv,
-    /// Global annotation/definition pairs
-    pub globals: HashMap<&'static str, (Option<RcValue>, RcType)>,
-    /// The type annotations of the binders we have passed over
-    pub claims: HashMap<FreeVar<String>, RcType>,
-    /// Any definitions we have passed over
-    pub definitions: HashMap<FreeVar<String>, RcTerm>,
-}
-
-impl Default for TcEnv {
-    fn default() -> TcEnv {
-        let universe0 = RcValue::from(Value::universe(0));
-        let true_value = RcValue::from(Value::Literal(Literal::Bool(true)));
-        let false_value = RcValue::from(Value::Literal(Literal::Bool(false)));
-        let bool_ty = RcValue::from(Value::global("Bool"));
-
-        TcEnv {
-            primitives: PrimEnv::default(),
-            globals: hashmap!{
-                "Bool" => (None, universe0.clone()),
-                "true" => (Some(true_value), bool_ty.clone()),
-                "false" => (Some(false_value), bool_ty.clone()),
-                "String" => (None, universe0.clone()),
-                "Char" => (None, universe0.clone()),
-                "U8" => (None, universe0.clone()),
-                "U16" => (None, universe0.clone()),
-                "U32" => (None, universe0.clone()),
-                "U64" => (None, universe0.clone()),
-                "I8" => (None, universe0.clone()),
-                "I16" => (None, universe0.clone()),
-                "I32" => (None, universe0.clone()),
-                "I64" => (None, universe0.clone()),
-                "F32" => (None, universe0.clone()),
-                "F64" => (None, universe0.clone()),
-                "Array" => (None, RcValue::from(Value::Pi(Scope::new(
-                    (Binder(FreeVar::fresh_unnamed()), Embed(RcValue::from(Value::global("U64")))),
-                    RcValue::from(Value::Pi(Scope::new(
-                        (Binder(FreeVar::fresh_unnamed()), Embed(universe0.clone())),
-                        universe0.clone(),
-                    ))),
-                )))),
-            },
-            claims: hashmap!{},
-            definitions: hashmap!{},
-        }
-    }
-}
 
 /// Type check and elaborate a module
 pub fn check_module(raw_module: &raw::Module) -> Result<Module, TypeError> {
