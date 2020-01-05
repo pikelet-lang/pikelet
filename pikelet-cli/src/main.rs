31c6c87dc404249ce38356c3c1633c46c6b8acc0
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2020-01-05 14:09:10 +1100

    First pass on a basic REPL implementation

--- pikelet-cli/src/main.rs
+++ pikelet-cli/src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use pikelet_cli::Options;
+use std::error::Error;
+use structopt::StructOpt;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    pikelet_cli::run(Options::from_args())
 }
