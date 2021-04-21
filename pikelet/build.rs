3ea367c89dae469241e0af17cc96a31d76d0d3c4
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2021-04-21 19:05:37 +1000

    Build script cleanups

--- pikelet/build.rs
+++ pikelet/build.rs
@@ -1,6 +1,5 @@
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     lalrpop::Configuration::new()
         .always_use_colors()
         .process_current_dir()
-        .unwrap();
 }
