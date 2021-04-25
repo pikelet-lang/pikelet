pikelet-lang/pikelet

commit 8fce091c0d5b0514261bcd0656b0a8b5a09e4860

Use `Vec`s for environments not used in evaluation
This should be more efficient as in these cases we don’t need the sharing that an `im::Vector` provides.

pikelet/src/pass/core_to_surface.rs
@@ -10,7 +10,7 @@
 use contracts::debug_ensures;
 use fxhash::FxHashMap;
 
-use crate::lang::core::{Constant, Globals, Locals, Term, TermData};
+use crate::lang::core::{Constant, Globals, Term, TermData};
 use crate::lang::surface;
 use crate::lang::Located;
 
@@ -18,7 +18,7 @@ use crate::lang::Located;
 pub struct Context<'me> {
     globals: &'me Globals,
     usages: FxHashMap<String, Usage>,
-    local_names: Locals<String>,
+    local_names: Vec<String>,
 }
 
 struct Usage {
@@ -48,7 +48,7 @@ impl<'me> Context<'me> {
         Context {
             globals,
             usages,
-            local_names: Locals::new(),
+            local_names: Vec::new(),
         }
     }
 
@@ -117,14 +117,14 @@ impl<'me> Context<'me> {
     ///
     /// [`core::Term`]: crate::lang::core::Term
     /// [`surface::Term`]: crate::lang::surface::Term
-    #[debug_ensures(self.local_names.size() == old(self.local_names.size()))]
+    #[debug_ensures(self.local_names.len() == old(self.local_names.len()))]
     pub fn from_term(&mut self, term: &Term) -> surface::Term {
         let term_data = match &term.data {
             TermData::Global(name) => match self.globals.get(name) {
                 Some(_) => surface::TermData::Name(name.to_owned()),
                 None => surface::TermData::Error, // TODO: Log error?
             },
-            TermData::Local(local_index) => match self.local_names.get(*local_index) {
+            TermData::Local(local_index) => match self.local_names.get(local_index.to_usize()) {
                 Some(name) => surface::TermData::Name(name.clone()),
                 None => surface::TermData::Error, // TODO: Log error?
             },

pikelet/src/lang/core.rs
@@ -200,9 +200,15 @@ impl Default for Globals {
 pub struct LocalIndex(u32);
 
 impl LocalIndex {
+    /// Convert the local index to a `usize`.
     pub fn to_usize(self) -> usize {
         self.0 as usize
     }
+}
+
+/// An infinite iterator of local indices.
+pub fn local_indices() -> impl Iterator<Item = LocalIndex> {
+    (0..).map(LocalIndex)
 }
 
 /// A de Bruijn level in the [local environment].
@@ -227,6 +233,7 @@ impl LocalIndex {
 pub struct LocalLevel(u32);
 
 impl LocalLevel {
+    /// Convert the local level to a `usize`.
     pub fn to_usize(self) -> usize {
         self.0 as usize
     }
@@ -250,7 +257,13 @@ impl LocalLevel {
 pub struct LocalSize(u32);
 
 impl LocalSize {
-    pub fn increment(self) -> LocalSize {
+    /// Convert the local size to a `usize`.
+    pub fn to_usize(self) -> usize {
+        self.0 as usize
+    }
+
+    /// Get the next size in the environment.
+    pub fn next_size(self) -> LocalSize {
         LocalSize(self.0 + 1)
     }
 
@@ -278,7 +291,10 @@ impl LocalSize {
     }
 }
 
-/// A local environment.
+/// A local environment, backed by a persistent vector.
+///
+/// Prefer mutating this in place, but if necessary this can be cloned in order
+/// to maintain a degree of sharing between copies.
 #[derive(Clone)]
 pub struct Locals<Entry> {
     /// The local entries that are currently defined in the environment.
@@ -322,24 +338,14 @@ impl<Entry: Clone> Locals<Entry> {
         self.entries.pop_back()
     }
 
-    /// Pop a number of entries off the environment.
-    pub fn pop_many(&mut self, count: usize) {
-        let len = self.entries.len().saturating_sub(count);
+    /// Truncate the environment to a given length.
+    pub fn truncate(&mut self, len: usize) {
         self.entries.truncate(len);
     }
 
     /// Clear the entries from the environment.
     pub fn clear(&mut self) {
         self.entries.clear();
-    }
-
-    /// Returns a reverse iterator over the entries in the environment and the
-    /// indices where those entries were bound.
-    pub fn iter_rev(&self) -> impl Iterator<Item = (LocalIndex, &Entry)> {
-        (self.entries.iter().rev()).scan(0, |current_index, entry| {
-            let local_index = LocalIndex(std::mem::replace(current_index, *current_index + 1));
-            Some((local_index, entry))
-        })
     }
 }
 

pikelet/src/lang/core/semantics.rs
@@ -504,7 +504,7 @@ pub fn read_back(globals: &Globals, local_size: LocalSize, unfold: Unfold, value
             let local = Arc::new(Value::local(local_size.next_level(), []));
             let input_type = read_back(globals, local_size, unfold, input_type);
             let output_type = output_closure.apply(globals, local);
-            let output_type = read_back(globals, local_size.increment(), unfold, &output_type);
+            let output_type = read_back(globals, local_size.next_size(), unfold, &output_type);
 
             Term::generated(TermData::FunctionType(
                 input_name_hint.clone(),
@@ -515,7 +515,7 @@ pub fn read_back(globals: &Globals, local_size: LocalSize, unfold: Unfold, value
         Value::FunctionTerm(input_name_hint, output_closure) => {
             let local = Arc::new(Value::local(local_size.next_level(), []));
             let output_term = output_closure.apply(globals, local);
-            let output_term = read_back(globals, local_size.increment(), unfold, &output_term);
+            let output_term = read_back(globals, local_size.next_size(), unfold, &output_term);
 
             Term::generated(TermData::FunctionTerm(
                 input_name_hint.clone(),
@@ -532,7 +532,7 @@ pub fn read_back(globals: &Globals, local_size: LocalSize, unfold: Unfold, value
                 types.push(Arc::new(entry_type));
 
                 let local_level = local_size.next_level();
-                local_size = local_size.increment();
+                local_size = local_size.next_size();
 
                 Arc::new(Value::local(local_level, []))
             });
@@ -548,7 +548,7 @@ pub fn read_back(globals: &Globals, local_size: LocalSize, unfold: Unfold, value
                 terms.push(Arc::new(entry_term));
 
                 let local_level = local_size.next_level();
-                local_size = local_size.increment();
+                local_size = local_size.next_size();
 
                 Arc::new(Value::local(local_level, []))
             });
@@ -618,7 +618,7 @@ fn is_equal_function_closure(
     let local = Arc::new(Value::local(local_size.next_level(), []));
     let term0 = closure0.apply(globals, local.clone());
     let term1 = closure1.apply(globals, local);
-    is_equal(globals, local_size.increment(), &term0, &term1)
+    is_equal(globals, local_size.next_size(), &term0, &term1)
 }
 
 /// Check that one record closure is equal to another record closure.
@@ -646,7 +646,7 @@ fn is_equal_record_closure(
         let local = Arc::new(Value::local(local_size.next_level(), []));
         locals0.push(local.clone());
         locals1.push(local);
-        local_size = local_size.increment();
+        local_size = local_size.next_size();
     }
 
     true

pikelet/src/lang/core/typing.rs
@@ -14,7 +14,7 @@ use crossbeam_channel::Sender;
 use std::sync::Arc;
 
 use crate::lang::core::semantics::{self, Elim, Unfold, Value};
-use crate::lang::core::{Constant, Globals, LocalSize, Locals, Term, TermData};
+use crate::lang::core::{Constant, Globals, LocalIndex, LocalSize, Locals, Term, TermData};
 use crate::reporting::{AmbiguousTerm, CoreTypingMessage, ExpectedType, Message};
 
 /// Type checking context.
@@ -22,7 +22,7 @@ pub struct Context<'me> {
     /// Global definition environment.
     globals: &'me Globals,
     /// Local type environment (used for getting the types of local variables).
-    local_declarations: Locals<Arc<Value>>,
+    local_declarations: Vec<Arc<Value>>,
     /// Local value environment (used for evaluation).
     local_definitions: Locals<Arc<Value>>,
     /// The diagnostic messages accumulated during type checking.
@@ -34,7 +34,7 @@ impl<'me> Context<'me> {
     pub fn new(globals: &'me Globals, message_tx: Sender<Message>) -> Context<'me> {
         Context {
             globals,
-            local_declarations: Locals::new(),
+            local_declarations: Vec::new(),
             local_definitions: Locals::new(),
             message_tx,
         }
@@ -43,6 +43,12 @@ impl<'me> Context<'me> {
     /// Get the size of the local environment.
     fn size(&self) -> LocalSize {
         self.local_definitions.size()
+    }
+
+    /// Get a local entry.
+    fn get_local(&self, local_index: LocalIndex) -> Option<&Arc<Value>> {
+        let local_level = self.size().index_to_level(local_index)?;
+        self.local_declarations.get(local_level.to_usize())
     }
 
     /// Push a local entry.
@@ -66,8 +72,9 @@ impl<'me> Context<'me> {
 
     /// Pop the given number of local entries.
     fn pop_many_locals(&mut self, count: usize) {
-        self.local_declarations.pop_many(count);
-        self.local_definitions.pop_many(count);
+        let len = self.size().to_usize().saturating_sub(count);
+        self.local_declarations.truncate(len);
+        self.local_definitions.truncate(len);
     }
 
     /// Report a diagnostic message.
@@ -101,12 +108,7 @@ impl<'me> Context<'me> {
 
     /// Read back a value into a normal form using the current state of the elaborator.
     pub fn read_back(&self, value: &Value) -> Term {
-        semantics::read_back(
-            self.globals,
-            self.local_definitions.size(),
-            Unfold::Never,
-            value,
-        )
+        semantics::read_back(self.globals, self.size(), Unfold::Never, value)
     }
 
     /// Check that one [`Value`] is computationally equal to another [`Value`].
@@ -115,11 +117,11 @@ impl<'me> Context<'me> {
     ///
     /// [`Value`]: crate::lang::core::semantics::Value
     pub fn is_equal(&self, value0: &Value, value1: &Value) -> bool {
-        semantics::is_equal(self.globals, self.local_definitions.size(), value0, value1)
+        semantics::is_equal(self.globals, self.size(), value0, value1)
     }
 
     /// Check that a term is a type.
-    #[debug_ensures(self.local_declarations.size() == old(self.local_declarations.size()))]
+    #[debug_ensures(self.local_declarations.len() == old(self.local_declarations.len()))]
     #[debug_ensures(self.local_definitions.size() == old(self.local_definitions.size()))]
     pub fn is_type(&mut self, term: &Term) -> bool {
         let r#type = self.synth_type(term);
@@ -137,7 +139,7 @@ impl<'me> Context<'me> {
     }
 
     /// Check that a term is an element of a type.
-    #[debug_ensures(self.local_declarations.size() == old(self.local_declarations.size()))]
+    #[debug_ensures(self.local_declarations.len() == old(self.local_declarations.len()))]
     #[debug_ensures(self.local_definitions.size() == old(self.local_definitions.size()))]
     pub fn check_type(&mut self, term: &Term, expected_type: &Arc<Value>) {
         match (&term.data, expected_type.force(self.globals)) {
@@ -239,7 +241,7 @@ impl<'me> Context<'me> {
     }
 
     /// Synthesize the type of a term.
-    #[debug_ensures(self.local_declarations.size() == old(self.local_declarations.size()))]
+    #[debug_ensures(self.local_declarations.len() == old(self.local_declarations.len()))]
     #[debug_ensures(self.local_definitions.size() == old(self.local_definitions.size()))]
     pub fn synth_type(&mut self, term: &Term) -> Arc<Value> {
         match &term.data {
@@ -252,7 +254,7 @@ impl<'me> Context<'me> {
                     Arc::new(Value::Error)
                 }
             },
-            TermData::Local(local_index) => match self.local_declarations.get(*local_index) {
+            TermData::Local(local_index) => match self.get_local(*local_index) {
                 Some(r#type) => r#type.clone(),
                 None => {
                     self.report(CoreTypingMessage::UnboundLocal);

pikelet/src/pass/surface_to_core.rs
@@ -22,7 +22,7 @@ pub struct Context<'me> {
     /// Global definition environment.
     globals: &'me core::Globals,
     /// Local type environment (used for getting the types of local variables).
-    local_declarations: core::Locals<(Option<String>, Arc<Value>)>,
+    local_declarations: Vec<(Option<String>, Arc<Value>)>,
     /// Local value environment (used for evaluation).
     local_definitions: core::Locals<Arc<Value>>,
     /// Distillation state (used for pretty printing).
@@ -36,7 +36,7 @@ impl<'me> Context<'me> {
     pub fn new(globals: &'me core::Globals, message_tx: Sender<Message>) -> Context<'me> {
         Context {
             globals,
-            local_declarations: core::Locals::new(),
+            local_declarations: Vec::new(),
             local_definitions: core::Locals::new(),
             core_to_surface: core_to_surface::Context::new(globals),
             message_tx,
@@ -50,12 +50,12 @@ impl<'me> Context<'me> {
 
     /// Get a local entry.
     fn get_local(&self, name: &str) -> Option<(core::LocalIndex, &Arc<Value>)> {
-        for (local_index, (decl_name, r#type)) in self.local_declarations.iter_rev() {
-            if decl_name.as_ref().map_or(false, |n| n == name) {
-                return Some((local_index, r#type));
-            }
-        }
-        None
+        Iterator::zip(core::local_indices(), self.local_declarations.iter().rev()).find_map(
+            |(index, (decl_name, r#type))| match decl_name {
+                Some(decl_name) if decl_name == name => Some((index, r#type)),
+                Some(_) | None => None,
+            },
+        )
     }
 
     /// Push a local entry.
@@ -82,8 +82,9 @@ impl<'me> Context<'me> {
 
     /// Pop the given number of local entries.
     fn pop_many_locals(&mut self, count: usize) {
-        self.local_declarations.pop_many(count);
-        self.local_definitions.pop_many(count);
+        let len = self.size().to_usize().saturating_sub(count);
+        self.local_declarations.truncate(len);
+        self.local_definitions.truncate(len);
         self.core_to_surface.pop_many_names(count);
     }
 
@@ -133,12 +134,7 @@ impl<'me> Context<'me> {
     /// [`Value`]: crate::lang::core::semantics::Value
     /// [`core::Term`]: crate::lang::core::Term
     pub fn read_back(&self, value: &Value) -> core::Term {
-        semantics::read_back(
-            self.globals,
-            self.local_definitions.size(),
-            Unfold::Never,
-            value,
-        )
+        semantics::read_back(self.globals, self.size(), Unfold::Never, value)
     }
 
     /// Check that one [`Value`] is computationally equal to another [`Value`].
@@ -147,7 +143,7 @@ impl<'me> Context<'me> {
     ///
     /// [`Value`]: crate::lang::core::semantics::Value
     pub fn is_equal(&self, value0: &Value, value1: &Value) -> bool {
-        semantics::is_equal(self.globals, self.local_definitions.size(), value0, value1)
+        semantics::is_equal(self.globals, self.size(), value0, value1)
     }
 
     /// Distill a [`core::Term`] into a [`surface::Term`].
@@ -171,7 +167,7 @@ impl<'me> Context<'me> {
     }
 
     /// Check that a term is a type, and return the elaborated term.
-    #[debug_ensures(self.local_declarations.size() == old(self.local_declarations.size()))]
+    #[debug_ensures(self.local_declarations.len() == old(self.local_declarations.len()))]
     #[debug_ensures(self.local_definitions.size() == old(self.local_definitions.size()))]
     pub fn is_type(&mut self, term: &Term) -> Option<core::Term> {
         let (core_term, r#type) = self.synth_type(term);
@@ -191,7 +187,7 @@ impl<'me> Context<'me> {
     }
 
     /// Check that a term is an element of a type, and return the elaborated term.
-    #[debug_ensures(self.local_declarations.size() == old(self.local_declarations.size()))]
+    #[debug_ensures(self.local_declarations.len() == old(self.local_declarations.len()))]
     #[debug_ensures(self.local_definitions.size() == old(self.local_definitions.size()))]
     pub fn check_type(&mut self, term: &Term, expected_type: &Arc<Value>) -> core::Term {
         match (&term.data, expected_type.force(self.globals)) {
@@ -396,7 +392,7 @@ impl<'me> Context<'me> {
     }
 
     /// Synthesize the type of a surface term, and return the elaborated term.
-    #[debug_ensures(self.local_declarations.size() == old(self.local_declarations.size()))]
+    #[debug_ensures(self.local_declarations.len() == old(self.local_declarations.len()))]
     #[debug_ensures(self.local_definitions.size() == old(self.local_definitions.size()))]
     pub fn synth_type(&mut self, term: &Term) -> (core::Term, Arc<Value>) {
         use std::collections::BTreeMap;
@@ -406,16 +402,13 @@ impl<'me> Context<'me> {
         match &term.data {
             TermData::Name(name) => {
                 if let Some((local_index, r#type)) = self.get_local(name.as_ref()) {
-                    return (
-                        core::Term::new(term.location, core::TermData::Local(local_index)),
-                        r#type.clone(),
-                    );
+                    let term_data = core::TermData::Local(local_index);
+                    return (core::Term::new(term.location, term_data), r#type.clone());
                 }
 
                 if let Some((r#type, _)) = self.globals.get(name.as_ref()) {
-                    let name = name.clone();
-                    let core_term = core::Term::new(term.location, core::TermData::Global(name));
-                    return (core_term, self.eval(r#type));
+                    let term_data = core::TermData::Global(name.clone());
+                    return (core::Term::new(term.location, term_data), self.eval(r#type));
                 }
 
                 self.report(SurfaceToCoreMessage::UnboundName {

commit ab46548029eeaff294b603fc912701ea9c44d63a

Fix name assignment in distiller
This fixes a name binding bug introduced in 8fce091c. We can’t just look up the names using the variable index directly when using a vector because new scopes are pushed onto the end of the environment - we need to convert to a level first!

pikelet/src/pass/core_to_surface.rs
@@ -10,7 +10,7 @@
 use contracts::debug_ensures;
 use fxhash::FxHashMap;
 
-use crate::lang::core::{Constant, EnvSize, Globals, Term, TermData};
+use crate::lang::core::{Constant, EnvSize, Globals, Term, TermData, VarIndex};
 use crate::lang::surface;
 use crate::lang::Located;
 
@@ -18,7 +18,7 @@ use crate::lang::Located;
 pub struct Context<'globals> {
     globals: &'globals Globals,
     usages: FxHashMap<String, Usage>,
-    var_names: Vec<String>,
+    names: Vec<String>,
 }
 
 struct Usage {
@@ -48,15 +48,24 @@ impl<'globals> Context<'globals> {
         Context {
             globals,
             usages,
-            var_names: Vec::new(),
+            names: Vec::new(),
         }
+    }
+
+    fn index_to_level(&self, index: VarIndex) -> usize {
+        let index = index.to_usize();
+        self.names.len().saturating_sub(index).saturating_sub(1)
+    }
+
+    fn get_name(&self, index: VarIndex) -> Option<&str> {
+        Some(self.names.get(self.index_to_level(index))?.as_str())
     }
 
     // FIXME: This is incredibly horrific and I do not like it!
     //
     // We could investigate finding more optimal optimal names by using free
     // variables, or look into [scope sets](https://typesanitizer.com/blog/scope-sets-as-pinata.html)
-    // for a more principled approach to scape names.
+    // for a more principled approach to scope names.
     pub fn push_scope(&mut self, name_hint: Option<&str>) -> String {
         let base_name = name_hint.unwrap_or(DEFAULT_NAME);
         let (fresh_name, base_name) = match self.usages.get_mut(base_name) {
@@ -87,12 +96,12 @@ impl<'globals> Context<'globals> {
         };
         // TODO: Reduce cloning of names
         self.usages.insert(fresh_name.clone(), usage);
-        self.var_names.push(fresh_name.clone());
+        self.names.push(fresh_name.clone());
         fresh_name
     }
 
     pub fn pop_scope(&mut self) {
-        if let Some(mut name) = self.var_names.pop() {
+        if let Some(mut name) = self.names.pop() {
             while let Some(base_name) = self.remove_usage(name) {
                 name = base_name;
             }
@@ -104,7 +113,7 @@ impl<'globals> Context<'globals> {
     }
 
     pub fn truncate_scopes(&mut self, count: EnvSize) {
-        (count.to_usize()..self.var_names.len()).for_each(|_| self.pop_scope());
+        (count.to_usize()..self.names.len()).for_each(|_| self.pop_scope());
     }
 
     fn remove_usage(&mut self, name: String) -> Option<String> {
@@ -124,15 +133,15 @@ impl<'globals> Context<'globals> {
     ///
     /// [`core::Term`]: crate::lang::core::Term
     /// [`surface::Term`]: crate::lang::surface::Term
-    #[debug_ensures(self.var_names.len() == old(self.var_names.len()))]
+    #[debug_ensures(self.names.len() == old(self.names.len()))]
     pub fn from_term(&mut self, term: &Term) -> surface::Term {
         let term_data = match &term.data {
             TermData::Global(name) => match self.globals.get(name) {
                 Some(_) => surface::TermData::Name(name.to_owned()),
                 None => surface::TermData::Error, // TODO: Log error?
             },
-            TermData::Var(index) => match self.var_names.get(index.to_usize()) {
-                Some(name) => surface::TermData::Name(name.clone()),
+            TermData::Var(index) => match self.get_name(*index) {
+                Some(name) => surface::TermData::Name(name.to_owned()),
                 None => surface::TermData::Error, // TODO: Log error?
             },
 