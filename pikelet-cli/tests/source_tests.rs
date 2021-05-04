dd0903905b072824eda5dfa93877ce60b0d47c30
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2021-05-04 21:03:32 +1000

    Automatically discover integration tests
    This uses the fantastic [libtest-mimic](https://lib.rs/crates/libtest-mimic) crate to build up a test harness dynamically. Later on we can add support for finer grained testing, for example adding underlines in comments for expected diagnostics, and snapshot tests for expected console output.

---
+++ pikelet-cli/tests/source_tests.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let args = libtest_mimic::Arguments::from_args();
+
+    std::env::set_current_dir("..").unwrap();
+
+    let tests = std::iter::empty()
+        .chain(pikelet_test::walk_files("examples").filter_map(pikelet_test::extract_test))
+        .chain(pikelet_test::walk_files("tests").filter_map(pikelet_test::extract_test))
+        .collect();
+    let run_test = pikelet_test::run_test(env!("CARGO_BIN_EXE_pikelet"));
+
+    libtest_mimic::run_tests(&args, tests, run_test).exit();
+}
